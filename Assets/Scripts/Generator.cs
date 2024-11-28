43a03c77edab2385546538964e3942b1514262cf
Sbalint959 <svenbali59@gmail.com>
2024-11-28 16:07:03 +0100

    added URP and simple login UI without functionality

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,4 +1,4 @@
-using original;
+// using original;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
--- Assets/Scripts/JSONPasrser.cs
+++ Assets/Scripts/JSONPasrser.cs
@@ -1,4 +1,4 @@
-﻿using original;
+﻿// using original;
 using System;
 using System.Collections;
 using UnityEngine;
